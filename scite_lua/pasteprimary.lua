ac3ae67d067eb6b8e15ca0ad701b0f704e302fee
mwhite <none@none>
2018-02-22 22:17:33 -0800

    fix paste primary bug; use Alt for block selection

--- scite_lua/pasteprimary.lua
+++ scite_lua/pasteprimary.lua
@@ -1,8 +1,10 @@
 -- Use xsel to read primary selection and paste it into editor
 -- use Shift+Ins shortcut to match terminal behavior
+-- spawner reference: https://github.com/mkottman/scite/tree/master/scite/custom/scite-debug
 
 function paste_primary()
-  local f = spawner.popen("xsel")
+  -- `timeout` necessary to prevent freeze if scite owns primary selection; `xsel -t 100` didn't work
+  local f = spawner.popen("timeout 1 xsel -o")
   local s = f:read("*a");
   f:close();
   if s ~= "" then
